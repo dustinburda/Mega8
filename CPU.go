53de1844553e31e14bc059176a98f143ca2ee410
dustinburda <dustin99wii@gmail.com>
2024-03-16 15:07:54 -0400

    Added Display struct, implemented ROM loading, CPU reset

--- CPU.go
+++ CPU.go
@@ -3,9 +3,11 @@ package main
 import (
 	// "bufio"
 	"fmt"
+	"os"
 	// "io"
-	// "os"
 )
+
+const PROGRAM_START uint16 = 0x200
 
 type CPU struct {
 	memory         [0xFFF]uint8
@@ -13,12 +15,47 @@ type CPU struct {
 	register_I     uint16
 	PC             uint16
 	stack          []uint16
+
+	delay_timer uint8
+	sound_timer uint8
+
+	display *Display
 }
 
-func (cpu *CPU) LOAD_ROM() {
-	fmt.Println("LOADING ROM")
+func (cpu *CPU) LOAD_ROM(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println("Failed to load ROM!")
+		return
+	}
+	defer file.Close()
+
+	file.Read(cpu.memory[PROGRAM_START:])
+
+}
+
+func (cpu *CPU) CPU_RESET() {
+	for i := 0; i < 0xFFF; i++ {
+		cpu.memory[i] = 0
+	}
+
+	for i := 0; i < 16; i++ {
+		cpu.data_registers[i] = 0
+	}
+
+	cpu.register_I = 0
+	cpu.PC = PROGRAM_START
+	clear(cpu.stack) //  = nil   Setting slice to nil will release the underlying memory to the garbage collection
+
+	cpu.display.CLEAR_DISPLAY()
 }
 
 func (cpu *CPU) CPU_INIT() {
 	fmt.Println("INIT")
+}
+
+func (cpu *CPU) MAIN_LOOP() {
+	for {
+		fmt.Println("RUNNING")
+	}
 }
--- Display.go
+++ Display.go
@@ -3,3 +3,15 @@ package main
 type Display struct {
 	buffer [64][32]byte
 }
+
+func (display *Display) CLEAR_DISPLAY() {
+	for i := 0; i < 64; i++ {
+		for j := 0; j < 32; j++ {
+			display.buffer[i][j] = 0
+		}
+	}
+}
+
+func (display *Display) SET_PIXEL(i uint8, j uint8, value byte) {
+	display.buffer[i][j] = value
+}
